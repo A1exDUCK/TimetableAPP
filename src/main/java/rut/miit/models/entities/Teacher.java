ed8c9e3ec086c9b5d08850f3d8cc0d1797f74b5f
AlexDUCK <leha.suchkov.2014@mail.ru>
2025-02-15 16:58:57 +0300

    Logs are working,problem with auth,add entities

--- src/main/java/rut/miit/models/entities/Teacher.java
+++ src/main/java/rut/miit/models/entities/Teacher.java
@@ -1,4 +1,14 @@
 package rut.miit.models.entities;
 
-public class Teacher {
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Teachers")
+public class Teacher extends rut.miit.models.entities.BaseEntity {
+    private String teacherNumber;
+    private String department;
+    private Set <teacherName> teacherNames;
 }
