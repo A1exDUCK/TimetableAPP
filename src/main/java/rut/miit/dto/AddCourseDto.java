41851244f5e7469ac29ac6df70cb05a243056292
AlexDUCK <leha.suchkov.2014@mail.ru>
2025-02-17 00:23:26 +0300

    fix addCourseDto

--- src/main/java/rut/miit/dto/AddCourseDto.java
+++ src/main/java/rut/miit/dto/AddCourseDto.java
@@ -1,35 +1,18 @@
 package rut.miit.dto;
 
 import jakarta.validation.constraints.*;
-import rut.miit.utils.validation.UniqueLessonConstraint;
+import rut.miit.utils.validation.UniqueCourseName;
 
-@UniqueLessonConstraint
+@UniqueCourseName
 public class AddCourseDto {
     private String courseName;
 
-    // Геттеры и сеттеры с базовыми ограничениями
-
-    @NotBlank(message = "Номер пары обязателен!")
-    public String getSelectedPair() {
-        return selectedPair;
+    @NotBlank(message = "Имя дисциплины обязательно!")
+    public String getCourseName() {
+        return courseName;
+    }
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
     }
 
-    @NotBlank(message = "Аудитория обязательна!")
-    @Size(max = 4, message = "Номер аудитории не длиннее 4 символов!")
-    public String getClassroom() {
-        return classroom;
-    }
-
-    // Сеттеры
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public void setSelectedPair(String selectedPair) {
-        this.selectedPair = selectedPair;
-    }
-
-    public void setClassroom(String classroom) {
-        this.classroom = classroom;
-    }
 }
